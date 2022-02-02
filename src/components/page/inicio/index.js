25d9fe9c67d27390adf79a8e48af80dd7e7bbb47
Manu Garcia <manuelalejandrogarcia@live.com.ar>
2022-02-02 20:33:08 -0300

    cambiado de estilos y cont clase 9

--- src/components/page/inicio/index.js
+++ src/components/page/inicio/index.js
@@ -5,12 +5,7 @@ import Portada from "images/inicio.jpg";
 export default function Inicio() {
     return (
         <div className="inicio">   
-            <Link to="/">
-            <h1>home</h1>
-            </Link>
-            <Link to="/productos">
-            <h1>Productos</h1>
-            </Link>
+            <h1>Tienda de zapatillas, lo mejor en calidad</h1>
             <img src={Portada} alt=""/>
         </div>
     )
--- src/components/page/productos/ProductosDetalles.js
+++ src/components/page/productos/ProductosDetalles.js
@@ -42,27 +42,26 @@ export const ProductosDetalles = () => {
     <>
     {
         <div className="detalles">
-          <h2>{detalle.title}</h2>
-          <p className="price">${detalle.price}</p>
-          <div className="grid">
-          <p className="nuevo">Nuevo</p>
-          <div className="tamano">
-            <select placeholder="Tamaño" >
-              <option value="1">1</option>
-              <option value="1">2</option>
-              <option value="1">3</option>
-              <option value="1">4</option>
-              <option value="1">5</option>
-              <option value="1">6</option>
-              <option value="1">7</option>
-              <option value="1">8</option>
-            </select>
-            <p>Tamaño</p>
-          </div>
-          </div>
-          <button onClick={() => addCarrito(detalle.id)}>
-            Añadir al carrito
-          </button>
+        <h2>{detalle.title}</h2>
+        <p className="price">${detalle.price}</p>
+        <div className="grid">
+        <div className="tamano">
+          <select placeholder="Tamaño" >
+            <option value="1">1</option>
+            <option value="1">2</option>
+            <option value="1">3</option>
+            <option value="1">4</option>
+            <option value="1">5</option>
+            <option value="1">6</option>
+            <option value="1">7</option>
+            <option value="1">8</option>
+          </select>
+          <p>Tamaño</p>
+        </div>
+        </div>
+        <button onClick={() => addCarrito(detalle.id)}>
+          Añadir al carrito
+        </button>
           
           {
             url ? <img src={images} alt={detalle.title}/> : <img src={detalle.image} alt={detalle.title}/>
